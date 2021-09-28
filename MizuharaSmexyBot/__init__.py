1c45616778e288992ed89ab9dba3c42ad5187cf7
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-09-28 20:22:17 +0530

    Update __init__.py

--- MizuharaSmexyBot/__init__.py
+++ MizuharaSmexyBot/__init__.py
@@ -15,14 +15,12 @@ is_config = path.exists("config.py")
 if is_config:
     from config import *
 else:
-    from sample_config import *
+    from MizuharaSmexyBot.sample_config import *
 
 USERBOT_PREFIX = USERBOT_PREFIX
 GBAN_LOG_GROUP_ID = GBAN_LOG_GROUP_ID
 SUDOERS = SUDO_USERS_ID
-WELCOME_DELAY_KICK_SEC = WELCOME_DELAY_KICK_SEC
 LOG_GROUP_ID = LOG_GROUP_ID
-MESSAGE_DUMP_CHAT = MESSAGE_DUMP_CHAT
 MOD_LOAD = []
 MOD_NOLOAD = []
 bot_start_time = time.time()
