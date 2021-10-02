88949c32e5166c35db93879cc43363bd3c4d21b6
Pain <86664546+AnuragSharma080@users.noreply.github.com>
2021-10-02 10:28:28 +0530

    Update http.py

--- MizuharaSmexyBot/utils/http.py
+++ MizuharaSmexyBot/utils/http.py
@@ -1,6 +1,6 @@
 from asyncio import gather
 
-from MizuharaSmexyBot import dispatcher
+from MizuharaSmexyBot.arqclient import aiohttpsession as session
 
 
 async def get(url: str, *args, **kwargs):
@@ -13,7 +13,7 @@ async def get(url: str, *args, **kwargs):
 
 
 async def head(url: str, *args, **kwargs):
-    async with dispatcher.head(url, *args, **kwargs) as resp:
+    async with session.head(url, *args, **kwargs) as resp:
         try:
             data = await resp.json()
         except Exception:
@@ -22,7 +22,7 @@ async def head(url: str, *args, **kwargs):
 
 
 async def post(url: str, *args, **kwargs):
-    async with dispatcher.post(url, *args, **kwargs) as resp:
+    async with session.post(url, *args, **kwargs) as resp:
         try:
             data = await resp.json()
         except Exception:
@@ -31,19 +31,25 @@ async def post(url: str, *args, **kwargs):
 
 
 async def multiget(url: str, times: int, *args, **kwargs):
-    return await gather(*[get(url, *args, **kwargs) for _ in range(times)])
+    return await gather(
+        *[get(url, *args, **kwargs) for _ in range(times)]
+    )
 
 
 async def multihead(url: str, times: int, *args, **kwargs):
-    return await gather(*[head(url, *args, **kwargs) for _ in range(times)])
+    return await gather(
+        *[head(url, *args, **kwargs) for _ in range(times)]
+    )
 
 
 async def multipost(url: str, times: int, *args, **kwargs):
-    return await gather(*[post(url, *args, **kwargs) for _ in range(times)])
+    return await gather(
+        *[post(url, *args, **kwargs) for _ in range(times)]
+    )
 
 
 async def resp_get(url: str, *args, **kwargs):
-    return await dispatcher.get(url, *args, **kwargs)
+    return await session.get(url, *args, **kwargs)
 
 
 async def resp_post(url: str, *args, **kwargs):
